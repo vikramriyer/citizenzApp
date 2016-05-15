b037558f7b9e6e23b991480ac0b8c6439952d38a
Mahalingam Iyer <mahalingam.m.iyer@gmail.com>
2016-05-15 23:00:22 +0530

    Final commit done

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ var posts=[
 	{
 		type:"poll",
 		id:1,
-		title:"20% hike in minimum wages",
+		title:"As a citizen, what is the biggest problem you face? ",
 		options:[
-			{title:"Yes",votes:0,value:true,percentVote:0},
-			{title:"No",votes:0,value:false,percentVote:0}
+			{title:"Traffic & Parking",votes:0,value:true,percentVote:10},
+			{title:"Cleanliness & Waste Management",votes:0,value:false,percentVote:15},
+			{title:"Water Supply",votes:0,value:false,percentVote:45},
+			{title:"Unsatisfactory response from civic authorities",votes:0,value:false,percentVote:2},
+			{title:"Pollution",votes:0,value:false,percentVote:28}
 			],
 		canSubmit:true,
 		isResultVisible:true,
@@ -17,10 +20,11 @@ var posts=[
 	{
 		type:"poll",
 		id:2,
-		title:"20% hike in minimum wages",
+		title:"Please select one of the Key Components as your top priority in Smart City",
 		options:[
-			{title:"Yes",votes:0,value:false,percentVote:80},
-			{title:"No",votes:0,value:false,percentVote:20}
+			{title:"Command and Control Center",votes:0,value:false,percentVote:60},
+			{title:"Intelligent Traffic Management Services",votes:0,value:false,percentVote:20},
+			{title:"Smart City Bus Services",votes:0,value:false,percentVote:20}
 			],
 		canSubmit:false,
 		isResultVisible:true,
@@ -30,21 +34,25 @@ var posts=[
 	{
 		type:"grievance",
 		id:3,
-		title:"Illegal encrochment at Miyapur",
+		title:"Recurrent fires at the dump have caused conditions unfit for habitation for residents of the adjacent area",
 		status:"Work In Progress",
 		progress:50,
 		canSubmit:true,
 		interactions:[
-			{text:"Yes, condition is worst need immediate action.",from:"Rahul Kumar",on:"20th Aug 2015"},
-			{text:"Yes, condition is worst need immediate action.",from:"Rahul Kumar",on:"20th Aug 2015"},
-			{text:"Yes, condition is worst need immediate action.",from:"Rahul Kumar",on:"20th Aug 2015"}
-		]
+			{text:"40 residents of Chembur went on a hunger strike to protest against the frequent fires and smoke",from:"Rahul Kumar",on:"20th Jan 2016"},
+			{text:"Neighbourhood surrounding to the dumping ground was identified as the city' most polluted area",from:"Manoj Sharma",on:"25th Jan 2016"},
+			{text:"GHMC had decided to close down a section of the dumping ground and use it to generate 7 to 8 MW of power by methane extraction, Effective from 20th April 2016.",from:"#PollutionControl",on:"20th Aug 2015"}
+		],
+		from: "Kishor Raj",
+		on: "15th Jan 2016"
 	},
 	{
 		type:"message",
 		id:4,
-		title:"Illegal encrochment has been removed  promptly at Miyapur",
-		isNotification:true
+		title:"Good job done by #GHMC by Cleaning Husain Sagar and Developing it into Clean and Precious tourist attraction. Keep it Up!",
+		isNotification:true,
+		from: "Anuresh Rathi",
+		on: "15th Feb 2016"
 	}
 ]
 var CitiesArray = [];
